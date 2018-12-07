c409e3749afd0c5379ea113e3c95279c05209e63
AlberBM96 <albertobm@protonmail.com>
2018-12-07 10:46:34 +0100

    Cleaner. Comments added. Not fully tested

--- Simulations/Matlab/beamforming_simu.m
+++ Simulations/Matlab/beamforming_simu.m
@@ -1,6 +1,6 @@
 %%*****---------------------------------------------------------------*****
 %{
-
+    - NOT FULLY TESTED!!
     - This script computes the value of the gain for a 2-element array when
     beamforming is applied.
 
@@ -17,52 +17,50 @@ alpha = input("Introduce beacon position, in degrees: ");
 r = input('Choose radius, in meters: ');
 
 %% Design antenna array
-f = 2.421e9;
-c = physconst('LightSpeed');
-lambda = c/f;
+% Calculate wave length and wave number
+f = 2.421e9;    % Operating frequency
+c = physconst('LightSpeed');    % Velocity of light
+lambda = c/f;   % Wavelength of the signal
+k0 = 2*pi/lambda;   % Wave number
 
-% Calculate the wave number 
-k0 = 2*pi/lambda;
+% Calculate patch dimensions
+epsilonR = 4.7; % Relative dielectric constant of FR4
+h = 0.0015; % Substrate thickness
+W = (lambda/2)*sqrt(2/(epsilonR + 1));  % Microstrip width
+epsilonEff = (epsilonR+1)/2 + (epsilonR-1)/2 * 1/(sqrt(1+12*h/W)); % Effective dielectric constant
+deltaL = h*0.412*((epsilonEff+0.3)*(W/h+0.264))/((epsilonEff-0.258)*(W/h+0.8)); % Length increment due to fringing
+L = c/(2*f*sqrt(epsilonEff)) - 2*deltaL; % Physical length of the patch
 
-% Calculate the Width of patch antenna
-epsylonr = 4.7;
-W = (lambda/2)*sqrt(2/(epsylonr + 1));
-
-% Epsilon Reff for W > h
-h = 0.0015;
-EpsylonReff = (epsylonr+1)/2 + (epsylonr-1)/2 * 1/(sqrt(1+12*h/W));
-
-% Extra length due to fringing DeltaL
-DeltaL = h*0.412*((EpsylonReff+0.3)*(W/h+0.264))/((EpsylonReff-0.258)*(W/h+0.8));
-
-% Physical length of Patch 
-PL = c/(2*f*sqrt(EpsylonReff))-2*DeltaL;
-
-% Impedence computation 
-Z = @(b) k0.*PL.*sin(b);
+% Calculate impedance and feeding point
+G1 = (W/(120*lambda))*(1-(k0*h)^2/24); % Single-slot conductance
+Z = @(b) k0.*L.*sin(b);
 X = @(b) ((sin(((k0*W)/2)*cos(b)))./cos(b)).^2.*besselj(0, Z(b)).*sin(b).^3;
-G12 =1/(120*pi^2) * integral(X, 0, pi);
+G12 =1/(120*pi^2) * integral(X, 0, pi); % Mutual conductance
+Rin = 50;   % Input impedance
+R0 = 1/(2*(G1+G12));    % Impedance at the origin
+FeedDistance = acos(sqrt(Rin/R0))*L/pi; % Feeding point
 
-% Single patch directivity
+% Calculate directivity
 I_1 = -2 + cos(k0*W) + k0*W*sinint(k0*W) + sin(k0*W)/(k0*W);
-G1 = I_1/(120*pi^2);
-Rin = 50;
-R0 = 1/(2*(G1+G12));
-FeedDistance = acos(sqrt(Rin/R0))*PL/pi;
+D_0 = (2*pi*W/lambda)^2*1/I_1; % Directivity of a single slot
+%G1 = I_1/(120*pi^2);
+g12 = G12/G1;
+D_AF = 2/(1 + g12); % Array factor of directivity
+D_2 = D_0*D_AF; % Directivity of two slots
 
 % Single patch simulation
 PatchAntenna = design(patchMicrostrip, f);
 PatchDielectric = dielectric('FR4');
 PatchDielectric.Thickness = h;
-PatchDielectric.EpsilonR = epsylonr;
+PatchDielectric.EpsilonR = epsilonR;
 PatchAntenna.Substrate = PatchDielectric;
+PatchAntenna.Length = W;    % Width and length are switched to rotate antennas
+PatchAntenna.Width = L;
 PatchAntenna.Height = h;
-PatchAntenna.Length = W;
-PatchAntenna.Width = PL;
-PatchAntenna.FeedOffset = [0 PL/2-FeedDistance];
+PatchAntenna.FeedOffset = [0 L/2-FeedDistance];
 
-% Microstrip array
-PatchSpacing = lambda/2;
+% Build array antenna
+PatchSpacing = lambda/2;    % Separation between elements
 PatchArray = linearArray('Element', PatchAntenna, 'ElementSpacing', PatchSpacing);
 
 %% Compute phase shift
