70e127e3f6ca31ce458440b98ee4ab48de834f43
Antonio Aguilera <Antonio@h154.aau1x-2.klient.frb7ab.site.aau.dk>
2018-12-07 14:12:25 +0100

    Added Serial Comm in GNU simulation, noise in Simulink simulation, TX and RX gain in GNU real_system

--- Simulations/gnuradio/epy_block_0.py
+++ Simulations/gnuradio/epy_block_0.py
@@ -8,23 +8,38 @@ be the parameters. All of them are required to have default values!
 
 import numpy as np
 from gnuradio import gr
+import serial
 
 
 class blk(gr.sync_block):  # other base classes are basic_block, decim_block, interp_block
-    """Embedded Python Block example - a simple multiply const"""
+    """ SUPER AWESOME SERIAL PORT
+    IT WORKS WITH MAGIK (AND SOME PYTHON)
+    COUNTER IS USED TO AVOID KILLING THE
+    POOR ARDUINO. BE CAREFUL """
 
-    def __init__(self):  # only default arguments here
+    def __init__(self, baud_rate=9600, port_name='/dev/cu.usbmodem1411', counter=100):  # only default arguments here
         """arguments to this function show up as parameters in GRC"""
         gr.sync_block.__init__(
             self,
-            name='Complex Inverse Cosine',   # will show up in GRC
+            name='Float Serial Sink',   # will show up in GRC
             in_sig=[np.float32],
-            out_sig=[np.complex64]
+            out_sig=[np.float32],
         )
         # if an attribute with the same name as a parameter is found,
         # a callback is registered (properties work, too).
+        self.serial_device=serial.Serial(port_name,baud_rate,timeout=1)
+        self.counter=counter
+        self.index=0
+
 
     def work(self, input_items, output_items):
         """example: multiply with constant"""
-        output_items[0][:] = np.arccos(np.complex64(input_items[0]))
+        self.index=self.index+1
+        
+        if(self.index>=self.counter):
+            self.index=0
+            avg_result=np.mean(input_items[0])
+            self.serial_device.write(str(avg_result))
+            
+        output_items[0][:] = input_items[0]
         return len(output_items[0])
--- epy_block_0.py
+++ epy_block_0.py
@@ -17,7 +17,7 @@ class blk(gr.sync_block):  # other base classes are basic_block, decim_block, in
         """arguments to this function show up as parameters in GRC"""
         gr.sync_block.__init__(
             self,
-            name='Complex Inverse Cosine',   # will show up in GRC
+            name='Complex acos',   # will show up in GRC
             in_sig=[np.float32],
             out_sig=[np.complex64]
         )
