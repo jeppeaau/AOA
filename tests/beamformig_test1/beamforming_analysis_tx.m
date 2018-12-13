33800b5de83d4830b32325c8e47fea82427b9acd
AlberBM96 <albertobm@protonmail.com>
2018-12-13 15:47:30 +0100

    Comments added. Minor fixes.

--- tests/beamformig_test1/beamforming_analysis_tx.m
+++ tests/beamformig_test1/beamforming_analysis_tx.m
@@ -7,6 +7,8 @@
     into the differen zones and generates a binary file for each of them. 
     - Complementary files needed: "bin2complex.m", "test1tx1.bin",
     "test1tx2.bin".
+    - Output file sintaxis: "data_aXzY_tx.bin" means Tx file for antenna X
+    corresponding to zone Y.
 
 %}
 %%*****---------------------------------------------------------------*****
@@ -17,20 +19,20 @@ close all
 clc
 
 %% Read data from GNU Radio output binaries
-input_files = ["test1tx1" "test1tx2"];
-f_in = zeros(length(input_files));
-data_tx = zeros(length(input_files), 22562070);
-for i=1:1:2
-    f_in(i) = fopen(input_files(i), 'r');
-    data_tx(i,:) = (real(bin2complex(f_in(i))))';
-    fclose(f_in(i));
+input_files = ["test1tx1" "test1tx2"];              % Files to be read
+f_in = zeros(length(input_files));                  % Pre-allocate memory
+data_tx = zeros(length(input_files), 22562070);     
+for i=1:1:length(input_files)                       % Iterate over number of files
+    f_in(i) = fopen(input_files(i), 'r');           % Open file
+    data_tx(i,:) = (real(bin2complex(f_in(i))))';   % Load binary data as complex float32. Transpose data.
+    fclose(f_in(i));                                % Close file
 end
 
 %% Plot raw data
 figure;
-for i=1:1:2
+for i=1:1:length(input_files)               % Iterate over number of antennas
     subplot(2,1,i)
-    plot(data_tx(i,:));
+    plot(data_tx(i,:));                     % Plot signals
     hold on
     grid on
     axis([0 length(data_tx(i,:)) 1.2*min(data_tx(i,:)) 1.2*max(data_tx(i,:))]);
@@ -41,16 +43,15 @@ for i=1:1:2
 end
 
 %% Divide datasets into zones
-zone_size = 5e3;
-%divs_rx1 = [1 2.3e6 3.95e6 5.63e6 7.1e6 7.9e6 9.27e6 10.8e6 12.1e6 13.5e6 14.9e6 16.4e6 17.8e6 18.7e6 20e6 21e6 22.3e6 length(data_rx1)];
-divs_tx = [1e6 3.95e6 7.2e6 9.5e6 13e6 15e6 18e6 21e6];
+zone_size = 5e3;                                            % 5000 samples per zone
+divs_tx = [1e6 3.95e6 7.2e6 9.5e6 13e6 15e6 18e6 21e6];     % Zone delimiters
 data_zones = zeros(length(divs_tx), zone_size, length(input_files));
-for i=1:1:2
+for i=1:1:length(input_files)        % Iterate over number of antennas
     figure
-    for j=1:1:8
-       data_zones(j,:,i) = data_tx(i, divs_tx(j):divs_tx(j) + zone_size-1);
+    for j=1:1:length(divs_tx)        % Iterate over number of zones
+       data_zones(j,:,i) = data_tx(i, divs_tx(j):divs_tx(j) + zone_size-1); % Store samples of corresponding zone
        subplot(4, 2, j)
-       plot(data_zones(j,:,i));
+       plot(data_zones(j,:,i));      % Plot siganls for each zone
        grid on
        axis([0 length(data_zones(j,:,i)) -0.12 0.12]);
        title("Tx Antenna " + num2str(i) + ", " + "Zone " + num2str(j));
@@ -60,13 +61,14 @@ for i=1:1:2
 end
 
 %% Write zone values to new binary file
-output_files = ["data11_tx.bin" "data12_tx.bin" "data13_tx.bin" "data14_tx.bin" "data15_tx.bin" "data16_tx.bin" "data17_tx.bin" "data18_tx.bin";
-                "data21_tx.bin" "data22_tx.bin" "data23_tx.bin" "data24_tx.bin" "data25_tx.bin" "data26_tx.bin" "data27_tx.bin" "data28_tx.bin"];
+% Names of the output files
+output_files = ["data_a1z1_tx.bin" "data_a1z2_tx.bin" "data_a1z3_tx.bin" "data_a1z4_tx.bin" "data_a1z5_tx.bin" "data_a1z6_tx.bin" "data_a1z7_tx.bin" "data_a1z8_tx.bin";
+                "data_a2z1_tx.bin" "data_a2z2_tx.bin" "data_a2z3_tx.bin" "data_a2z4_tx.bin" "data_a2z5_tx.bin" "data_a2z6_tx.bin" "data_a2z7_tx.bin" "data_a2z8_tx.bin"];
 f_out = zeros(length(divs_tx), length(divs_tx));
-for i=1:1:2
+for i=1:1:length(input_files)
     for j=1:1:length(divs_tx)
-        f_out(i,j) = fopen(output_files(i,j), 'w');
-        fwrite(f_out(i,j), data_zones(j,:,i), 'float32');
-        fclose(f_out(i,j));
+        f_out(i,j) = fopen(output_files(i,j), 'w');         % Open file
+        fwrite(f_out(i,j), data_zones(j,:,i), 'float32');   % Write data to file
+        fclose(f_out(i,j));                                 % Close file
     end
 end
