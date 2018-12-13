edbc1d7b9ab25ca9135a9de80a90ab93baf8a453
AlberBM96 <albertobm@protonmail.com>
2018-12-13 15:46:51 +0100

    Comments added. Minor fixes.

--- tests/beamformig_test1/beamforming_analysis_rx.m
+++ tests/beamformig_test1/beamforming_analysis_rx.m
@@ -7,6 +7,8 @@
     into the different zones and generates a binary file for each of them.
     - Complementary files needed: "bin2complex.m", "test1antenna1.bin",
     "test1antenna2.bin".
+    - Output file sintaxis: "data_aXzY_rx.bin" means Rx file for antenna X
+    corresponding to zone Y.
 
 %}
 %%*****---------------------------------------------------------------*****
@@ -17,20 +19,20 @@ close all
 clc
 
 %% Read data from GNU Radio output binaries
-input_files = ["test1antenna1" "test1antenna2"];
-f_in = zeros(length(input_files));
+input_files = ["test1antenna1" "test1antenna2"];    % Files to be read
+f_in = zeros(length(input_files));                  % Pre-allocate memory
 data_rx = zeros(length(input_files), 22478519);
-for i=1:1:2
-    f_in(i) = fopen(input_files(i), 'r');
-    data_rx(i,:) = (real(bin2complex(f_in(i))))';
-    fclose(f_in(i));
+for i=1:1:length(input_files)                       % Iterate over number of files
+    f_in(i) = fopen(input_files(i), 'r');           % Open file
+    data_rx(i,:) = (real(bin2complex(f_in(i))))';   % Load binary data as complex float32. Transpose data.
+    fclose(f_in(i));                                % Close file
 end
 
 %% Plot raw data
 figure;
-for i=1:1:2
+for i=1:1:length(input_files)       % Iterate over number of antennas
     subplot(2,1,i)
-    plot(data_rx(i,:));
+    plot(data_rx(i,:));             % Plot signals
     hold on
     grid on
     axis([0 length(data_rx(i,:)) 1.2*min(data_rx(i,:)) 1.2*max(data_rx(i,:))]);
@@ -41,16 +43,15 @@ for i=1:1:2
 end
 
 %% Divide datasets into zones
-zone_size = 5e3;
-%divs_rx1 = [1 2.3e6 3.95e6 5.63e6 7.1e6 7.9e6 9.27e6 10.8e6 12.1e6 13.5e6 14.9e6 16.4e6 17.8e6 18.7e6 20e6 21e6 22.3e6 length(data_rx1)];
-divs_rx = [1e6 3.95e6 7.2e6 9.5e6 13e6 15e6 18e6 21e6];
+zone_size = 5e3;                                            % 5000 samples per zone
+divs_rx = [1e6 3.95e6 7.2e6 9.5e6 13e6 15e6 18e6 21e6];     % Zone delimiters
 data_zones = zeros(length(divs_rx), zone_size, length(input_files));
-for i=1:1:2
+for i=1:1:length(input_files)           % Iterate over number of antennas
     figure
-    for j=1:1:8
-       data_zones(j,:,i) = data_rx(i, divs_rx(j):divs_rx(j) + zone_size-1);
+    for j=1:1:length(divs_rx)           % Iterate over number of zones
+       data_zones(j,:,i) = data_rx(i, divs_rx(j):divs_rx(j) + zone_size-1); % Store samples of corresponding zone
        subplot(4, 2, j)
-       plot(data_zones(j,:,i));
+       plot(data_zones(j,:,i));         % Plot siganls for each zone
        grid on
        axis([0 length(data_zones(j,:,i)) -0.12 0.12]);
        title("Rx Antenna " + num2str(i) + ", " + "Zone " + num2str(j));
@@ -60,13 +61,14 @@ for i=1:1:2
 end
 
 %% Write zone values to new binary file
-output_files = ["data11_rx.bin" "data12_rx.bin" "data13_rx.bin" "data14_rx.bin" "data15_rx.bin" "data16_rx.bin" "data17_rx.bin" "data18_rx.bin";
-                "data21_rx.bin" "data22_rx.bin" "data23_rx.bin" "data24_rx.bin" "data25_rx.bin" "data26_rx.bin" "data27_rx.bin" "data28_rx.bin"];
+% Names of the output files
+output_files = ["data_a1z1_rx.bin" "data_a1z2_rx.bin" "data_a1z3_rx.bin" "data_a1z4_rx.bin" "data_a1z5_rx.bin" "data_a1z6_rx.bin" "data_a1z7_rx.bin" "data_a1z8_rx.bin";
+                "data_a2z1_rx.bin" "data_a2z2_rx.bin" "data_a2z3_rx.bin" "data_a2z4_rx.bin" "data_a2z5_rx.bin" "data_a2z6_rx.bin" "data_a2z7_rx.bin" "data_a2z8_rx.bin"];
 f_out = zeros(length(divs_rx), length(divs_rx));
 for i=1:1:2
     for j=1:1:length(divs_rx)
-        f_out(i,j) = fopen(output_files(i,j), 'w');
-        fwrite(f_out(i,j), data_zones(j,:,i), 'float32');
-        fclose(f_out(i,j));
+        f_out(i,j) = fopen(output_files(i,j), 'w');         % Open file
+        fwrite(f_out(i,j), data_zones(j,:,i), 'float32');   % Write data to file
+        fclose(f_out(i,j));                                 % Close file
     end
 end
